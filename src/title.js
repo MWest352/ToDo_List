aea5822878de55d264be9a9592f256a453753d30
MWest352 <MWest35285@gmail.com>
2023-04-13 15:45:55 -0600

    Place and center logo and text for main title section

--- src/title.js
+++ src/title.js
@@ -9,7 +9,7 @@ function titleSection() {
  
   titleSection.classList.add('title_section');
   titleSection.appendChild(titleHeader);
-  // titleHeader.appendChild(titleLogo);
+  titleHeader.appendChild(titletext);
   titleSection.appendChild(newList);
   titleSection.appendChild(titleSubheader);
   titleSection.appendChild(titleFooter);
@@ -25,8 +25,8 @@ function createTitle(text, className) {
 }
 
 //DIV CREATION
-const titleHeader = createTitle("Checkmark!", 'title_header');
-// const titleLogo = createTitle("L", 'title_Logo')
+const titleHeader = createTitle("", 'title_header');
+const titletext = createTitle("Checkmark!", 'title_text')
 const titleSubheader = createTitle("Welcome to Checkmark, your new personal assistant!", 'title_subheader');
 const newList = createTitle("Start New Checklist!", 'title_subheader');
 const titleFooter = createTitle("Footer", 'title_footer');
