031b953d0d09572f3025b3892d9bcfbaf7f162c6
MWest352 <MWest35285@gmail.com>
2023-06-09 18:21:43 -0600

    update form for overlay creation

--- src/checklistForm.js
+++ src/checklistForm.js
@@ -1,1 +1,30 @@
 //Checklist Form Generation
+
+//IMPORT DIV GENERATOR
+import createDivWithClass from "./divGenerator"
+
+export default
+
+//CREATE OVERLAY
+function overlay() {
+  const overlay = document.createElement('div');
+ 
+  overlay.classList.add('overlay_main');
+  overlay.appendChild(formContainer);
+  
+  return overlay;
+}
+
+//CREATE FORM FIELD
+function createForm(text, className) {
+  const form = createDivWithClass(className);
+  form.innerHTML = text;
+  return form;
+}
+
+//FILL FORM FIELD TEST
+const formContainer = createForm("Hello World", 'form_container');
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import './Styling/style.css';
 import _ from 'date-fns'
 import titleSection from './title';
+import overlay from './checklistForm';
 
 function component() {
   const element = document.createElement('div');
   
 
   element.classList.add('title');
-  element.appendChild(titleSection())
+  element.appendChild(titleSection());
+  
 
   return element;
 }
 
 document.body.appendChild(component());
+document.body.appendChild(overlay());
